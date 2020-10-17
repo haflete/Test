377ae2716c10d37945ee640c6e298226ea19e534
Nedal Zaideh <nalzaydeh@dg-cash.com>
2020-10-17 19:03:35 +0300

    init commit

---
+++ runnableContainter/src/main/java/com/dc/runnableContainter/RunnableContainterApplication.java
@@ -0,0 +1,13 @@
+package com.dc.runnableContainter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnableContainterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnableContainterApplication.class, args);
+	}
+
+}
---
+++ runnableContainter/src/test/java/com/dc/runnableContainter/RunnableContainterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.runnableContainter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnableContainterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
